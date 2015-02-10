6ca7c21eccfcd75c29618f14a05894ef01646a4f
skepticfx <ahamed.nafeez@gmail.com>
2015-02-09 18:24:52 -0800

    ui: re-origanize ui code

--- src/js/hookish.js
+++ src/js/hookish.js
@@ -57,27 +57,6 @@ $(function(){
   });
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 
 
 function stripped(data, len){
