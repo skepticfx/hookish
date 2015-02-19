0a40ed6ed23a20297a0630067b64417b03769504
skepticfx <ahamed.nafeez@gmail.com>
2015-02-19 13:53:56 -0800

    patch release

--- src/js/hookish.js
+++ src/js/hookish.js
@@ -58,14 +58,17 @@ $(function(){
 });
 
 
+var Hookish = {
 
-function stripped(data, len){
-  if(len <=10) return data;
-  if(data.length <= len)  return data;
-  return data.substr(0,len-4) + " ...";
+  // Match sink.score(source)
+  isDomFlow: function(source, sink){
+    return (sink.score(source) > 0.3)
+  }
+
+
 }
 
 
-function addToTable(stat){
-  $('#domssTableBody').prepend('<tr><td><strong>' + stat.nature + '</strong></td><td>' + stat.type + '</td><td title="'+stat.data.replace(/"/gi,'%22')+'">'+ stripped(stat.data,50) +'</td><td>'+stat.href+'</td></tr>');
-}
+
+
+
