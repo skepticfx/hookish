7b1c61fae4a60c864721752a5795861d980872fc
Tom Turner <turnertom@gmail.com>
2018-02-11 12:42:01 +0000

    Update to latest version of wshook as old version misses events

--- src/js/libsToInject.js
+++ src/js/libsToInject.js
@@ -193,66 +193,81 @@ var libsToInject = {
       // Mutable MessageEvent.
       // Subclasses MessageEvent and makes data, origin and other MessageEvent properites mutatble.
       function MutableMessageEvent(o) {
-        this.bubbles = o.bubbles || false;
-        this.cancelBubble = o.cancelBubble || false;
-        this.cancelable = o.cancelable || false;
-        this.currentTarget = o.currentTarget || null;
-        this.data = o.data || null;
-        this.defaultPrevented = o.defaultPrevented || false;
-        this.eventPhase = o.eventPhase || 0;
-        this.lastEventId = o.lastEventId || "";
-        this.origin = o.origin || "";
-        this.path = o.path || new Array(0);
-        this.ports = o.parts || new Array(0);
-        this.returnValue = o.returnValue || true;
-        this.source = o.source || null;
-        this.srcElement = o.srcElement || null;
-        this.target = o.target || null;
-        this.timeStamp = o.timeStamp || null;
-        this.type = o.type || "message";
-        this.__proto__ = o.__proto__ || MessageEvent.__proto__;
+        this.bubbles = o.bubbles || false
+        this.cancelBubble = o.cancelBubble || false
+        this.cancelable = o.cancelable || false
+        this.currentTarget = o.currentTarget || null
+        this.data = o.data || null
+        this.defaultPrevented = o.defaultPrevented || false
+        this.eventPhase = o.eventPhase || 0
+        this.lastEventId = o.lastEventId || ''
+        this.origin = o.origin || ''
+        this.path = o.path || new Array(0)
+        this.ports = o.parts || new Array(0)
+        this.returnValue = o.returnValue || true
+        this.source = o.source || null
+        this.srcElement = o.srcElement || null
+        this.target = o.target || null
+        this.timeStamp = o.timeStamp || null
+        this.type = o.type || 'message'
+        this.__proto__ = o.__proto__ || MessageEvent.__proto__
       }
 
-      var before = wsHook.before = function(data, url) {
+      var before = wsHook.before = function (data, url) {
         return data;
       };
-      var after = wsHook.after = function(e, url) {
+      var after = wsHook.after = function (e, url) {
         return e;
       };
-      wsHook.resetHooks = function() {
+      wsHook.resetHooks = function () {
         wsHook.before = before;
         wsHook.after = after;
       }
 
       var _WS = WebSocket;
-      WebSocket = function(url, protocols) {
+      WebSocket = function (url, protocols) {
         var WSObject;
         this.url = url;
         this.protocols = protocols;
-        if (!this.protocols)
+        if (!this.protocols) {
           WSObject = new _WS(url);
-        else
+	} else { 
           WSObject = new _WS(url, protocols);
-
+	}
         var _send = WSObject.send;
-        var _wsobject = this;
-        WSObject.send = function(data) {
-          data = wsHook.before(data, WSObject.url) || data;
+        WSObject.send = function (data) {
+          arguments[0] = wsHook.before(data, WSObject.url) || data;
           _send.apply(this, arguments);
         }
+   
+	// Events needs to be proxied and bubbled down.
+        WSObject._addEventListener = WSObject.addEventListener
+        WSObject.addEventListener = function () {
+          var eventThis = this;
+          // if eventName is 'message'
+          if (arguments[0] === 'message') {
+            arguments[1] = (function (userFunc) {
+            return function instrumentAddEventListener () {
+              arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url) || arguments[0];
+              userFunc.apply(eventThis, arguments);
+            }
+          })(arguments[1])
+        }
+        return WSObject._addEventListener.apply(this, arguments)
+        }
 
-        // Events needs to be proxied and bubbled down.
-        var onmessageFunction;
-        WSObject.__defineSetter__('onmessage', function(func) {
-          onmessageFunction = func;
-        });
-        WSObject.addEventListener('message', function(e) {
-          e = new wsHook.after(new MutableMessageEvent(e), this.url) || e;
-          e = new MessageEvent(e.type, e);
-          onmessageFunction.apply(this, [e])
-        });
-
-        return WSObject;
+        Object.defineProperty(WSObject, 'onmessage', {
+          'set': function () {
+            var eventThis = this;
+            var userFunc = arguments[0];
+            var onMessageHandler = function () {
+              arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url) || arguments[0]
+              userFunc.apply(eventThis, arguments)
+            }
+            WSObject._addEventListener.apply(this, ['message', onMessageHandler, false])
+          }
+        })
+        return WSObject
       }
     })();
   },
